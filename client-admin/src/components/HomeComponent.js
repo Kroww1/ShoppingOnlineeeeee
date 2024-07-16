190d1d0bfbf925bba1f30e6e5269947dda0a319b
Kroww1 <128805740+Kroww1@users.noreply.github.com>
2024-07-16 10:50:51 +0700

    Add files via upload

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img src="https://media1.tenor.com/m/t3eKwU-odDgAAAAd/sui-siu.gif" width="800px" height="600px" alt="" />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster1.vyujhbj.mongodb.net',
+    DB_USER: 'bot1',
+    DB_PASS: 'abc123456',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'NguyenQuocCuong',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'cuongnguyenquoc1211@gmail.com', // gmail service
+    EMAIL_PASS: 'swhm bymr hguq tmpe'
+  };
+  module.exports = MyConstants;
